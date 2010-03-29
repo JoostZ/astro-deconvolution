b5b4cdadd24eb4b0f289bb6d16cafb6680dc7df6
zalmstra <zalmstra@xs4all.nl>
2010-03-29 19:48:39 +0000

    Added NUnit tests for Gaussian

--- AstroDeconvolution/Gaussian.cs
+++ AstroDeconvolution/Gaussian.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using NUnit.Framework;
+
 namespace AstroDeconvolution
 {
     /**
@@ -40,6 +42,8 @@ namespace AstroDeconvolution
             SigmaX = sigmaX;
             SigmaY = sigmaY;
             Theta = theta;
+            CosTheta = Math.Cos(_theta * ToRad);
+            SinTheta = Math.Sin(_theta * ToRad);
             Scale = scale;
         }
 
@@ -117,7 +121,7 @@ namespace AstroDeconvolution
             }
             set
             {
-                if (_sigmaX != value)
+                if (_sigmaY != value)
                 {
                     _sigmaY = value;
                     SigmaY2 = value * value;
@@ -331,5 +335,139 @@ namespace AstroDeconvolution
         }
 
         #endregion
+
+        public struct BoundingBox
+        {
+            public double XMin;
+            public double XMax;
+            public double YMin;
+            public double YMax;
+        }
+
+        public BoundingBox GetBound(double value)
+        {
+            BoundingBox result = new BoundingBox();
+
+            double scale = Math.Log(Scale / value);
+            // Find a_x
+            double a_x = SigmaX * Math.Sqrt(2 * scale);
+            double a_y = SigmaY * Math.Sqrt(2 * scale);
+
+            double t = Math.Atan2(-a_y * SinTheta, a_x * CosTheta);
+            double cosT = Math.Cos(t);
+            double sinT = Math.Sin(t);
+            double x1 = X0 + a_x * cosT * CosTheta - a_y * sinT * SinTheta;
+            double x2 = X0 - a_x * cosT * CosTheta + a_y * sinT * SinTheta;
+            if (x1 > x2)
+            {
+                result.XMax = x1;
+                result.XMin = x2;
+            }
+            else
+            {
+                result.XMax = x2;
+                result.XMin = x1;
+            }
+
+            t = Math.Atan2(a_y * CosTheta, a_x * SinTheta);
+            cosT = Math.Cos(t);
+            sinT = Math.Sin(t);
+            double y1 = Y0 + a_x * cosT * SinTheta + a_y * sinT * CosTheta;
+            double y2 = Y0 - a_x * cosT * SinTheta - a_y * sinT * CosTheta;
+            if (y1 > y2)
+            {
+                result.YMax = y1;
+                result.YMin = y2;
+            }
+            else
+            {
+                result.YMax = y2;
+                result.YMin = y1;
+            }
+
+            return result;
+        }
     }
+#if DEBUG
+    [TestFixture]
+    public class GaussianTest
+    {
+        [Test]
+        public void TestProperties()
+        {
+            double x0 = 0.0;
+            double y0 = 0.0;
+            double sigmaX = 1.0;
+            double sigmaY = 1.0;
+            double theta = 0.0;
+            double scale = 1.0;
+
+            Gaussian theGaussian = new Gaussian(x0, y0, sigmaX, sigmaY, theta, scale);
+            Assert.That(theGaussian.X0, Is.EqualTo(x0).Within(0.00001), "Initial x0");
+            Assert.That(theGaussian.Y0, Is.EqualTo(y0).Within(0.00001), "Initial y0");
+            Assert.That(theGaussian.SigmaX, Is.EqualTo(sigmaX).Within(0.00001), "Initial sigma X");
+            Assert.That(theGaussian.SigmaY, Is.EqualTo(sigmaY).Within(0.00001), "Initial sigma Y");
+            Assert.That(theGaussian.Theta, Is.EqualTo(theta).Within(0.00001), "Initial theta");
+            Assert.That(theGaussian.Scale, Is.EqualTo(scale).Within(0.00001), "Initial scale");
+
+            x0 += 10;
+            theGaussian.X0 = x0;
+            Assert.That(theGaussian.X0, Is.EqualTo(x0).Within(0.00001), "Changed x0");
+
+            y0 += 10;
+            theGaussian.Y0 = y0;
+            Assert.That(theGaussian.Y0, Is.EqualTo(y0).Within(0.00001), "Changed y0");
+
+            sigmaX += 0.5;
+            theGaussian.SigmaX = sigmaX;
+            Assert.That(theGaussian.SigmaX, Is.EqualTo(sigmaX).Within(0.00001), "Changed sigma X");
+
+            sigmaY += 0.5;
+            theGaussian.SigmaY = sigmaY;
+            Assert.That(theGaussian.SigmaY, Is.EqualTo(sigmaY).Within(0.00001), "Changed sigma Y");
+
+            theta += 30;
+            theGaussian.Theta = theta;
+            Assert.That(theGaussian.Theta, Is.EqualTo(theta).Within(0.00001), "Changed theta");
+
+            scale += 0.5;
+            theGaussian.Scale = scale;
+            Assert.That(theGaussian.Scale, Is.EqualTo(scale).Within(0.00001), "Changed scale");
+
+            double[] parameters = theGaussian.ParameterValues;
+
+            Assert.That(theGaussian.X0, Is.EqualTo(parameters[(int)Gaussian.Parameters.X0]).Within(0.00001), "Parameter X0");
+            Assert.That(theGaussian.Y0, Is.EqualTo(parameters[(int)Gaussian.Parameters.Y0]).Within(0.00001), "Parameter Y0");
+            Assert.That(theGaussian.SigmaX, Is.EqualTo(parameters[(int)Gaussian.Parameters.SigmaX]).Within(0.00001), "Parameter SigmaX");
+            Assert.That(theGaussian.SigmaY, Is.EqualTo(parameters[(int)Gaussian.Parameters.SigmaY]).Within(0.00001), "Parameter SigmaY");
+            Assert.That(theGaussian.Theta, Is.EqualTo(parameters[(int)Gaussian.Parameters.Theta]).Within(0.00001), "Parameter Theta");
+            Assert.That(theGaussian.Scale, Is.EqualTo(parameters[(int)Gaussian.Parameters.Scale]).Within(0.00001), "Parameter Scale");
+        }
+
+
+        [Test]
+        public void TestValues()
+        {
+            double x0 = 0.0;
+            double y0 = 0.0;
+            double sigmaX = 1.0;
+            double sigmaY = 1.0;
+            double theta = 0.0;
+            double scale = 1.0;
+
+            Gaussian theGaussian = new Gaussian(x0, y0, sigmaX, sigmaY, theta, scale);
+
+            for (double x = x0; x < 5 * sigmaX; x += 0.2)
+            {
+                double expectedValue = scale * Math.Exp(-0.5 * (x - x0) * (x - x0) / (sigmaX * sigmaX));
+                Assert.That(theGaussian[x, 0], Is.EqualTo(expectedValue).Within(0.00001), "Value [" + x + ", 0]");
+            }
+            for (double y = y0; y < 5 * sigmaY; y += 0.2)
+            {
+                double expectedValue = scale * Math.Exp(-0.5 * (y - y0) * (y - y0) / (sigmaY * sigmaY));
+                Assert.That(theGaussian[0, y], Is.EqualTo(expectedValue).Within(0.00001), "Value [" + 0 + ", y]");
+            }
+        }
+    }
+#endif
 }
