c3eff11ac4acffcdc99fbddfc61ad44b43006fba
zalmstra@xs4all.nl <zalmstra@xs4all.nl@fb7ad29e-2459-11df-8ea0-49ba94de29ab>
2010-03-29 19:48:39 +0000

    Added NUnit tests for Gaussian

---
+++ trunk/NUnitDebug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Gui;
+namespace NUnitDebug
+{
+    class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return NUnit.Gui.AppEntry.Main(args);
+        }
+    }
+}
